0f7a3236e9369e23cb914d08972db4fc4fe50c99
Tornike <tokobayern@gmail.com>
2016-05-29 16:31:29 +0400

    MCQ

---
+++ src/MultipleChoiceQuestion.java
@@ -0,0 +1,10 @@
+import javax.swing.JRadioButton;
+
+public class MultipleChoiceQuestion {
+	private String correctAnswer = "";
+	private JRadioButton choices;
+	
+	public MultipleChoiceQuestion(String answer){
+		choices = new JRadioButton();
+	}
+}
