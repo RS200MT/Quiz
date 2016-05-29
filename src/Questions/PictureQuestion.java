cca90aae64776d2fdfc7deeec1018fa633275b4e
Tornike <tokobayern@gmail.com>
2016-05-29 17:36:48 +0400

    update

---
+++ src/Questions/PictureQuestion.java
@@ -0,0 +1,11 @@
+package Questions;
+
+public class PictureQuestion {
+	private String imgSrc = "";
+	private String answer = "";
+	
+	public PictureQuestion(String url, String answer){
+		this.imgSrc = url;
+		this.answer = answer;
+	}
+}
