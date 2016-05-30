390cab7b2c4be3b6222d1348a5cac88461c9fbd1
MeryChon <mchon14@freeuni.edu.ge>
2016-05-30 16:14:50 +0400

    QuestionUpdates

--- src/Questions/MultipleChoiceQuestion.java
+++ src/Questions/MultipleChoiceQuestion.java
@@ -5,11 +5,12 @@ import java.util.List;
 import javax.swing.JRadioButton;
 
 public class MultipleChoiceQuestion {
+	private String question = "";
 	private String correctAnswer = "";
 	private List<String> choices;
-	
-	
-	public MultipleChoiceQuestion(String answer, List<String> questions){
+		
+	public MultipleChoiceQuestion(String question, String answer, List<String> questions){
+		this.question = question;
 		choices = new ArrayList<String>(questions);
 		correctAnswer = answer;
 	}
@@ -17,4 +18,5 @@ public class MultipleChoiceQuestion {
 	public String getAnswer(){
 		return correctAnswer;
 	}
+	
 }
--- src/Questions/PictureQuestion.java
+++ src/Questions/PictureQuestion.java
@@ -1,10 +1,12 @@
 package Questions;
 
 public class PictureQuestion {
+	private String question = "";
 	private String imgSrc = "";
 	private String answer = "";
 	
-	public PictureQuestion(String url, String answer){
+	public PictureQuestion(String question, String url, String answer){
+		this.question = question;
 		this.imgSrc = url;
 		this.answer = answer;
 	}
