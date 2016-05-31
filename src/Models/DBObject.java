691f9d9289daeb9fe868cb490fe295de525b3e0b
MeryChon <mchon14@freeuni.edu.ge>
2016-05-31 21:35:12 +0400

    addUser and userAlreadyExists updated

--- src/Models/DBObject.java
+++ src/Models/DBObject.java
@@ -1,5 +1,7 @@
 package Models;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -59,6 +61,8 @@ public class DBObject {
 		return result;
 	}
 	
+	
+	
 	private void executeUpdate(String query) {
 		Connection conn = getConnection();
 		try {
@@ -76,16 +80,43 @@ public class DBObject {
 	 * Adds new user into users table. Uses executeUpdate;
 	 * @param u
 	 */
-	public void addUser(String name, String email, String password) {
+	public boolean addUser(String name, String email, String password) {
 		Connection conn = getConnection();
-		java.util.Date date= new java.util.Date();
-		String query = "INSERT INTO " + TABLE_USERS + " VALUES " + 
-						"(" + name + ", "+email + ", " + password + ", " + new Timestamp(date.getTime()) +");";
-		executeUpdate(query);
-		closeConnection(conn);
+		if(userAlreadyExists(name, email)) {
+			closeConnection(conn);
+			return false;
+		} else {
+			java.util.Date date= new java.util.Date();
+			String query = "INSERT INTO " + TABLE_USERS + " VALUES " + 
+							"(" + name + ", "+email + ", " + password + ", " + new Timestamp(date.getTime()) +");";
+			executeUpdate(query);
+			closeConnection(conn);
+			return true;
+		}
 	}
 	
-	
+
+	/**
+	 * Checks if user with given name or email already exists;
+	 * @param name
+	 * @param email
+	 * @return boolean
+	 */
+	private boolean userAlreadyExists(String name, String email) {
+		String queryForName = "SELECT * FROM " + TABLE_USERS + "WHERE user_name is " + name;
+		String queryForEmail = "SELECT * FROM " + TABLE_USERS + "WHERE email is " + email;
+		ResultSet r1 = getResultSet(queryForName);
+		ResultSet r2 = getResultSet(queryForEmail);
+		try {
+			if(!r1.next() || !r2.next()){
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return true;
+	}
+
 	public String getPasswordHash(String userName) throws SQLException {
 		String query = " Select * from users where user_name = \"" + userName + "\"";
 		ResultSet rs = getResultSet(query);
