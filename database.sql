60bbac4bf29b86dd4b0a14af08de46bfe71dd06e
waska14 <vchad14@freeuni.edu.ge>
2016-05-31 15:34:07 +0400

    UTF8 all tables

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ create table if not exists friends(
 	user1_id int,
     user2_id int,
     status varchar(50)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 create table if not exists quizes (
 	id int auto_increment primary key,
@@ -16,7 +16,7 @@ create table if not exists question_images(
     question_id int,
     image_url varchar(255),
     primary key(id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 create table if not exists questions (
 	id int auto_increment primary key,
