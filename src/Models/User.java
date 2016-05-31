01c5a532ef228246b4e4ef482b254663511efe83
waska14 <vchad14@freeuni.edu.ge>
2016-05-31 16:57:40 +0400

    user Class

---
+++ src/Models/User.java
@@ -0,0 +1,41 @@
+package Models;
+
+public class User {
+	private int id;
+	private String userName;
+	private String email;
+	private String regDate;
+	private int quizNumber;
+	
+	public User(int id, String userName, String email, String regDate, int quizNumber) {
+		this.id = id;
+		this.userName = userName;
+		this.email = email;
+		this.regDate = regDate;
+		this.quizNumber = quizNumber;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getRegDate() {
+		return this.regDate;
+	}
+	
+	public int getQuizNumber() {
+		return this.quizNumber;
+	}
+	
+	public void increaseQuizNumber() {
+		
+	}
+}
