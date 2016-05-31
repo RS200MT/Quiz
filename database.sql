aebf9aa4bd312ffae5869d160cd4c081053d3663
MeryChon <mchon14@freeuni.edu.ge>
2016-05-31 15:25:07 +0400

    Commit before pull

--- database.sql
+++ database.sql
@@ -1,24 +1,26 @@
-DROP SCHEMA IF EXISTS quiz_site_database;
-CREATE SCHEMA quiz_site_database;
+CREATE SCHEMA IF NOT EXISTS quiz_site_database;
 USE quiz_site_database;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS correct_answers;
+drop table if exists users;
+drop table if exists correct_answers;
 
-CREATE TABLE users (
-	id int not null auto_increment, -- Could use varchar
+CREATE TABLE IF NOT EXISTS users (
+	id int not null auto_increment primary key, -- Could use varchar
     user_name varchar(60) not null,
     email varchar(45) not null,
-    password varchar(50) not null, -- May need change of variable type
+    password char(40) not null, -- variable type ??
     reg_date timestamp,
     quizes_written int DEFAULT 0,
     
-    primary key (id)
-);
+    key(user_name),
+    key(email)    
+)ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
 
-CREATE TABLE correct_answers(
+CREATE TABLE IF NOT EXISTS correct_answers(
 	id int not null auto_increment primary key,
     question_id int not null, 
-    correct_answer text -- May need change of variable type
-);
+    correct_answer text, -- May need change of variable type
+    
+    key(question_id)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
 
