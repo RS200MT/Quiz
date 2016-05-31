cc30fcd2312a673abe671f0049d141e13c6403aa
Tornike <tokobayern@gmail.com>
2016-05-31 15:02:13 +0400

    friends Dand question images

--- database.sql
+++ database.sql
@@ -1,24 +1,12 @@
-DROP SCHEMA IF EXISTS quiz_site_database;
-CREATE SCHEMA quiz_site_database;
-USE quiz_site_database;
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS correct_answers;
-
-CREATE TABLE users (
-	id int not null auto_increment, -- Could use varchar
-    user_name varchar(60) not null,
-    email varchar(45) not null,
-    password varchar(50) not null, -- May need change of variable type
-    reg_date timestamp,
-    quizes_written int DEFAULT 0,
-    
-    primary key (id)
+create table if not exists friends(
+	user1_id int,
+    user2_id int,
+    status varchar(50)
 );
 
-CREATE TABLE correct_answers(
-	id int not null auto_increment primary key,
-    question_id int not null, 
-    correct_answer text -- May need change of variable type
+create table if not exists question_images(
+	id int auto_increment,
+    question_id int,
+    image_url varchar(255),
+    primary key(id)
 );
-
