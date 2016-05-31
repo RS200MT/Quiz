bdf3893ec073fe66ca9ac953c14f49ed32ed2bc6
waska14 <vchad14@freeuni.edu.ge>
2016-05-31 15:44:38 +0400

    MySQL.java class for db constants

--- database.sql
+++ database.sql
@@ -1,9 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS quiz_site_database;
-USE quiz_site_database;
-
-drop table if exists users;
-drop table if exists correct_answers;
-
 CREATE TABLE IF NOT EXISTS users (
 	id int not null auto_increment primary key, -- Could use varchar
     user_name varchar(60) not null,
@@ -14,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     
     key(user_name),
     key(email)    
-)ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS correct_answers(
 	id int not null auto_increment primary key,
@@ -22,7 +16,7 @@ CREATE TABLE IF NOT EXISTS correct_answers(
     correct_answer text, -- May need change of variable type
     
     key(question_id)
-)ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
 
 create table if not exists friends(
 	user1_id int,
---
+++ src/database/MySQL.java
@@ -0,0 +1,8 @@
+package database;
+
+public class MySQL {
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "waska";
+	public static final String MYSQL_DATABASE_SERVER = "localhost:4000";
+	public static final String MYSQL_DATABASE_NAME = "products";
+}
