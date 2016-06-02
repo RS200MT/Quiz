17b04dbaaf27afdc697f5470ddfb3b1293e3d316
Tornike <tokobayern@gmail.com>
2016-06-02 14:33:03 +0400

    check

--- src/Models/DBInfo.java
+++ src/Models/DBInfo.java
@@ -4,5 +4,5 @@ public class DBInfo {
 	public static final String MYSQL_USERNAME = "root";
 	public static final String MYSQL_PASSWORD = "root";
 	public static final String MYSQL_DATABASE_SERVER = "127.0.0.1:3306";
-	public static final String MYSQL_DATABASE_NAME = "sys";
+	public static final String MYSQL_DATABASE_NAME = "syss";
 }
