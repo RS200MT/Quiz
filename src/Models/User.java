dc2e88a293de6fbf215db23828fca33502ea6a1f
waska14 <vchad14@freeuni.edu.ge>
2016-06-02 13:22:40 +0400

    Unused imports delete

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,8 +1,5 @@
 package Models;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public class User {
 	private int id;
 	private String userName;
@@ -40,28 +37,5 @@ public class User {
 
 	public void increaseQuizNumber() {
 		this.quizNumber++;
-	}
-
-	public static String hexToString(byte[] bytes) {
-		StringBuffer buff = new StringBuffer();
-		for (int i = 0; i < bytes.length; i++) {
-			int val = bytes[i];
-			val = val & 0xff; // remove higher bits, sign
-			if (val < 16)
-				buff.append('0'); // leading 0
-			buff.append(Integer.toString(val, 16));
-		}
-		return buff.toString();
-	}
-
-	public static String getHash(String password) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA");
-			md.update(password.getBytes());
-			return hexToString(md.digest());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return "";
 	}
 }
