28a02dd01a4426b07189228c093630fe49336da7
waska14 <vchad14@freeuni.edu.ge>
2016-06-10 17:31:52 +0400

    Quiz.java

--- src/Models/Quiz.java
+++ src/Models/Quiz.java
@@ -1,37 +1,30 @@
 package Models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import Questions.Question;
 
 public class Quiz {
 	private int id;
-	private String author; // User author?
+	private String author;
 	private List<Question> questions;
+	private HashMap<Integer, String> userAnswers;
 	private int timesWritten;
+	private int currentQuestion;
 	
-	public Quiz(int id, String author) {
+	public Quiz(int id, String author, int timesWritten) {
 		this.id = id;
 		this.author = author;
-		this.questions = new ArrayList<Question>();
-		this.timesWritten = 0;
-	}
-	
-	public Quiz(int id, String author, List<Question> questions, int timesWritten) {
-		this.id = id;
-		this.author = author;
-		this.questions = questions;
 		this.timesWritten = timesWritten;
+		this.questions = new ArrayList<Question>();
+		this.userAnswers = new HashMap<Integer, String>();
+		this.currentQuestion = 0;
 	}
-	
 	
 	public void addQuestion(Question q) {
 		this.questions.add(q);
-	}
-	
-	public void IncreaseTimesWrittenBy(int num) {
-		this.timesWritten += num;
 	}
 	
 	public int getID() {
@@ -49,4 +42,27 @@ public class Quiz {
 	public int getTimesWritten() {
 		return this.timesWritten;
 	}
+	
+	public int getCurrentIndex(){
+		return currentQuestion;
+	}
+	
+	public void setAnswer(int questionIndex, String answer) {
+		userAnswers.put(questionIndex, answer);
+	}
+	
+	public boolean hasMoreQuestions() {
+		return currentQuestion < questions.size();
+	}
+	
+	public Question getQuestion() {
+		return questions.get(currentQuestion++);
+	}
+	
+	public void randomizeQuestions() {
+		//TODO
+	}
+	
+	
+	
 }
